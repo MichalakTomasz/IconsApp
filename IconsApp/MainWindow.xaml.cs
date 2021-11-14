2a05f5db0c198f9cf1662477f232dc7f151273a0
Tomasz Michalak <vlad3dracula@gmail.com>
2021-11-14 20:15:00 +0100

    Add project files.

---
+++ IconsApp/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using IconsApp.Services;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IconsApp
+{
+    public partial class MainWindow : Window
+    {
+        private string _path;
+        private IconService _iconService;
+        private MenuItem _menuItem;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _iconService = new IconService();
+        }
+
+        private void otworzButton_Click(object sender, RoutedEventArgs e)
+        {
+            var open = new OpenFileDialog();
+            open.ShowDialog();
+            _path = open.FileName;
+            var images = _iconService.GetIcons(_path);
+
+
+            images.ToList().ForEach(bitmapSource =>
+            {
+                var menuItem = new MenuItem { DataContext = bitmapSource.image, Header = "Zapisz" };
+                menuItem.Click += MenuItem_Click;
+                wrapperImages.Children.Add(
+                    new Image
+                    {
+                        Source = bitmapSource.image,
+                        Width = 64,
+                        Height = 64,
+                        DataContext = bitmapSource.nr,
+                        ContextMenu = new ContextMenu { ItemsSource = new[] { menuItem } }
+                    });
+                menuItem.Tag = bitmapSource.image;
+            });
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var img = (e.OriginalSource as MenuItem).Tag as BitmapSource;
+            var saveDialog = new SaveFileDialog();
+            saveDialog.ShowDialog();
+            using var stream = new FileStream(saveDialog.FileName, FileMode.Create);
+            var encoder = new PngBitmapEncoder();
+            var bitmapSource = img;
+            encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+            encoder.Save(stream);
+        }
+    }
+}
---
+++ IconsApp/Services/IconService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace IconsApp.Services
+{
+    public class IconService
+    {
+        [System.Runtime.InteropServices.DllImport("shell32.dll")]
+        public static extern IntPtr ExtractIcon(IntPtr hInst, string lpszExeFileName, int nIconIndex);
+        [System.Runtime.InteropServices.DllImport("shell32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern int ExtractIconEx(string stExeFileName, int nIconIndex, IntPtr[] phiconLarge, IntPtr[] phiconSmall, int nIcons);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool DestroyIcon(IntPtr hIcon);
+        public object GetAssociatedIcon(string path)
+        => Icon.ExtractAssociatedIcon(path);
+
+        public IEnumerable<(int nr, BitmapSource image)> GetIcons(string path)
+        {
+            var iconsCount = ExtractIconEx(path, 0, null, null, 0);
+            var images = new List<(int, BitmapSource)>();
+            Enumerable.Range(0, iconsCount).ToList().ForEach(i =>
+            {
+                var licon = new IntPtr[1];
+                ExtractIconEx(path, i, licon, null, 1);
+                var icon = Icon.FromHandle(licon[0]);
+                var imageSource = Imaging.CreateBitmapSourceFromHIcon(licon[0], new System.Windows.Int32Rect(), BitmapSizeOptions.FromEmptyOptions());
+                DestroyIcon(licon[0]);
+                images.Add((i, imageSource));
+            });
+
+            return images;
+        }
+
+    }
+}
